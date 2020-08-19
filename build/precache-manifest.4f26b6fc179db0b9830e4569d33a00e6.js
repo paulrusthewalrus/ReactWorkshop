33a9d27c9a6a6c9158bdfe85ac1e4b59a323de50
Paul Clarke <paulioclarke@icloud.com>
2020-08-19 10:23:43 +1000

    Build Folder for TicTacToe
    Our final compiled build for our tic tac toe game.

---
+++ build/precache-manifest.4f26b6fc179db0b9830e4569d33a00e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fcc77697016046054de8a4f8a1bc7c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13a78149060dc88b1acd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d7e87c5eefb413678f44",
+    "url": "/static/js/2.4ec9cda9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ec9cda9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a78149060dc88b1acd",
+    "url": "/static/js/main.b7f9dbeb.chunk.js"
+  },
+  {
+    "revision": "df40ba753f8222cfb8ed",
+    "url": "/static/js/runtime-main.0cd3ba68.js"
+  }
+]);
